a1cc8e6341d75f09291d0136716e09141f189185
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-03-03 14:47:41 -0500

    l8/1 - react/next setup

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <main>
+            <h1>About this Site</h1>
+            <p>This is using Next.js and React.</p>
+        </main>
+    )
+}
---
+++ app/movies/page.tsx
@@ -0,0 +1,8 @@
+export default function Movies() {
+    return (
+        <main>
+            <h1>Our Movies</h1>
+            
+        </main>
+    );
+}
