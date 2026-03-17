663097dba7d8a9651cc087d506e9f833d7dfd061
roshanprasai <200598344@student.georgianc.on.ca>
2026-03-17 17:47:13 -0400

    Add PUT method for movie update

--- app/api/movies/[id]/route.ts
+++ app/api/movies/[id]/route.ts
@@ -1,29 +1,68 @@
 // api routes for /api/movies/{id}: GET (one), PUT, DELETE
 
-export async function GET(req: Request, { params }: { params: Promise<{ id: string }> }) {
-    // get id from url param
-    const { id } = await params;
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  // get id from url param
+  const { id } = await params;
 
-    // call GET (one) in server api
-    const res: Response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/v1/movies/${id}`);
+  // call GET (one) in server api
+  const res: Response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/v1/movies/${id}`
+  );
 
-    if (!res.ok) throw new Error('Failed to fetch movie');
+  if (!res.ok) throw new Error("Failed to fetch movie");
 
-    // return movie data
-    return Response.json(await res.json());
+  // return movie data
+  return Response.json(await res.json());
 }
 
-export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }> }) {
-    // get id from url param
-    const { id } = await params;
+export async function PUT(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  // get id from url param
+  const { id } = await params;
 
-    // call GET (one) in server api
-    const res: Response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/v1/movies/${id}`, {
-        method: 'DELETE'
-    });
+  // get request body
+  const body = await req.json();
 
-    if (!res.ok) throw new Error('Failed to delete movie');
+  // call PUT in server api
+  const res: Response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/v1/movies/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
 
-    // return success response, there is no movie data
-    return new Response(null, { status: 204 });
+  if (!res.ok) throw new Error("Failed to update movie");
+
+  // return updated movie data
+  return Response.json(await res.json());
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  // get id from url param
+  const { id } = await params;
+
+  // call DELETE in server api
+  const res: Response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/v1/movies/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+
+  if (!res.ok) throw new Error("Failed to delete movie");
+
+  // return success response, there is no movie data
+  return new Response(null, { status: 204 });
 }
