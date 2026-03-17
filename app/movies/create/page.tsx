4e8c5ac0d0d85e73d166d34fcb7abd424d13f966
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-03-17 14:52:36 -0400

    l10/1 - begin state hook

--- app/movies/create/page.tsx
+++ app/movies/create/page.tsx
@@ -1,13 +1,54 @@
+'use client';
+import { useState } from "react";
+
 export default function CreateMovie() {
+    // state vars for form inputs
+    const [title, setTitle] = useState<string>('');
+
+    // state var key/val dictionary of validation errors in form
+    const [errors, setErrors] = useState<Record<string, string>>({});
+
+    const validate = (): boolean => {
+        const newErrors: Record<string, string> = {};
+
+        // check title
+        if (!title.trim()) newErrors.title = 'Title is required';
+
+        setErrors(newErrors);
+
+        // are there any errors in our dictionary?
+        if (Object.keys(newErrors).length === 0) {
+            alert('valid');
+            return true;
+        }
+
+        alert('invalid');
+        return false;
+    };
+
     return (
         <main>
             <h1>Movie Details</h1>
-            <form>
-                <fieldset><label htmlFor="">Title: *</label><input type="text" /></fieldset>
-                <fieldset><label htmlFor="">Year: *</label><input type="text" /></fieldset>
-                <fieldset><label htmlFor="">Genre: </label><input type="text" /></fieldset>
-                <fieldset><label htmlFor="">Rating /10:</label><input type="text" /></fieldset>
-                <fieldset><label htmlFor="">Duration (mins)</label><input type="text" /></fieldset>
+            <form onSubmit={validate}>
+                <fieldset>
+                    <label htmlFor="">Title: *</label>
+                    <input name="title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="">Year: *</label>
+                    <input type="text" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="">Genre: </label>
+                    <input type="text" />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="">Rating /10:</label>
+                    <input type="text" /></fieldset>
+                <fieldset>
+                    <label htmlFor="">Duration (mins)</label>
+                    <input type="text" />
+                </fieldset>
                 <button>Save</button>
             </form>
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+'use client';
+import { useState } from "react";
+
 export default function Home() {
   const year: number = 2026;
+
+  // numeric state var with setter.  value persists on re-render
+  const [counter, setCounter] = useState<number>(0);
 
   return (
     <main>
        <h1>Node Flix</h1>
       <p>We are building this for C0MP2068 in Winter {year}.</p>
+      <button onClick={(e) => setCounter(counter +1)}>Click Me</button>
+      <p>You&apos;ve clicked {counter} times.</p>
     </main>
   );
 }
