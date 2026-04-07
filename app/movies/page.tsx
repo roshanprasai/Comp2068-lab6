8c8e6c0374e573148133cfdaf5c0f5e667dee59d
roshanprasai <200598344@student.georgianc.on.ca>
2026-04-07 12:07:45 -0400

    Fix movies API path for deployment

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,34 +1,43 @@
-import Link from "next/link";
+export const dynamic = 'force-dynamic';
 
+import Link from "next/link";
 import { Movie } from "@/types/movie";
 
 async function getMovies(): Promise<Movie[]> {
-    // call api layer which calls server to get movie data
-    const res: Response = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/movies`);
+  const res: Response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/movies`,
+    {
+      cache: "no-store",
+    }
+  );
 
-    if (!res.ok) throw new Error('Failed to fetch Movies');
-    
-    const movies: Movie[] = await res.json();
-    return movies;
+  if (!res.ok) {
+    throw new Error("Failed to fetch movies");
+  }
+
+  const movies: Movie[] = await res.json();
+  return movies;
 }
 
 export default async function Movies() {
-    // try to fetch movie data from route / server api
-    const movies = await getMovies();
+  const movies = await getMovies();
 
-    return (
-        <main>
-            <h1>Our Movies</h1> 
-            <Link href="/movies/create" className="standardLink">Add a New Movie</Link>
-            <ul>
-                {movies.map((movie) => (
-                    <li className="card" key={movie._id}>
-                        <Link href={`/movies/${movie._id}`}>
-                            {movie.title}
-                        </Link>
-                    </li>
-                ))}    
-            </ul>           
-        </main>
-    );
+  return (
+    <main>
+      <h1>Our Movies</h1>
+      <Link href="/movies/create" className="standardLink">
+        Add a New Movie
+      </Link>
+
+      <ul>
+        {movies.map((movie) => (
+          <li className="card" key={movie._id}>
+            <Link href={`/movies/${movie._id}`}>
+              {movie.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
